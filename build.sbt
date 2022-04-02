ff09fd17b3d50efac3fad89a114b117412a53f1d
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-03 01:42:00 +0200

    add metrics (#1)

--- build.sbt
+++ build.sbt
@@ -5,7 +5,11 @@ ThisBuild / scalaVersion := "3.1.1"
 lazy val root = (project in file("."))
   .settings(
     name := "zio-http-example",
+    resolvers +=
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
     libraryDependencies ++= Seq(
-      "io.d11" %% "zhttp" % "2.0.0-RC4"
+      "io.d11" %% "zhttp" % "2.0.0-RC4",
+      "com.github.pjfanning" %% "zio-metrics-micrometer" % "0.20.0-SNAPSHOT" changing(),
+      "io.micrometer" %  "micrometer-registry-prometheus" % "1.8.4"
     )
   )
--- src/main/scala/example/HelloWorld.scala
+++ src/main/scala/example/HelloWorld.scala
@@ -1,15 +1,37 @@
 package example
 
+import com.github.pjfanning.zio.micrometer.Counter
+import com.github.pjfanning.zio.micrometer.safe.{Counter, Registry}
+import io.micrometer.prometheus.{PrometheusConfig, PrometheusMeterRegistry}
 import zhttp.http.*
 import zhttp.service.Server
-import zio.ZIOAppDefault
+import zio.{Clock, ZIO, ZIOAppDefault}
 
 object HelloWorld extends ZIOAppDefault {
 
+  private val registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+  private val metricEnv = Clock.live ++ Registry.makeWith(registry)
+
+  private def recordCount(method: String, path: String) = {
+    for {
+      c <- Counter.labelled("http", Some("HTTP counts"), Seq("method", "path"))
+      result <- c(Seq("get", "text")).inc
+    } yield result
+  }
+
   // Create HTTP route
-  val app: HttpApp[Any, Nothing] = Http.collect[Request] {
-    case Method.GET -> !! / "text" => Response.text("Hello World!")
-    case Method.GET -> !! / "json" => Response.json("""{"greetings": "Hello World!"}""")
+  val app: HttpApp[Any, Nothing] = Http.collectZIO[Request] {
+    case Method.GET -> !! / "text" => {
+      ZIO.succeed(Response.text("Hello World!")).zipPar(
+        recordCount("get", "text").provideLayer(metricEnv))
+    }
+    case Method.GET -> !! / "json" => {
+      ZIO.succeed(Response.json("""{"greetings": "Hello World!"}""")).zipPar(
+        recordCount("get", "json").provideLayer(metricEnv))
+    }
+    case Method.GET -> !! / "metrics" => {
+      ZIO.succeed(Response.text(registry.scrape()))
+    }
   }
 
   // Run it like any simple app
